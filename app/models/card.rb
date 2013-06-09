4d1b222297cf4e6f88621395e21730b3b88ab32b
pedrogrande <peter.argent@gmail.com>
2013-06-09 23:42:27 +1000

    card model fix

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ActiveRecord::Base
   belongs_to :user
-  has_many :card_colors, :card_font_colors, :card_font_families, :card_images
+  has_many :card_colors
+  has_many :card_font_colors
+  has_many :card_font_families
+  has_many :card_images
 end
